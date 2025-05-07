073a630e973d329ab5f6f2b3aba2b2b4a64a580c
KJ Roelke <kroelke@choctawnation.com>
2025-05-07 10:53:30 -0500

    cleanup phpcs errors

--- wp-content/themes/ATIP/page.php
+++ wp-content/themes/ATIP/page.php
@@ -11,14 +11,9 @@ $page_content = get_field( 'page_content' );
 
 <div id="content" class="site-content container py-5 mt-5">
 	<div id="primary" class="content-area">
-
-
-
 		<div class="row">
 			<div class="col-12">
-
 				<main id="main" class="site-main">
-
 					<div class="entry-content">
 						<!-- Featured Image -->
 						<?php the_post_thumbnail(); ?>
@@ -26,40 +21,27 @@ $page_content = get_field( 'page_content' );
 						<?php the_title( '<h1>', '</h1>' ); ?>
 						<!-- Content -->
 						<?php echo $page_content; ?>
-
 						<?php
-
-						// Check value exists.
-						if ( have_rows( 'content' ) ) :
-
-							// Loop through rows.
-							while ( have_rows( 'content' ) ) :
+						if ( have_rows( 'content' ) ) {
+							while ( have_rows( 'content' ) ) {
 								the_row();
-
-								// Case: Content layout.
-								if ( get_row_layout() == 'text_content' ) :
+								if ( 'text_content' === get_row_layout() ) {
 									$content = get_sub_field( 'content' );
 									echo $content;
-
-									// Case: Image layout.
-								elseif ( get_row_layout() == 'image_content' ) :
-									$title = get_sub_field( 'title' );
-									$image = get_sub_field( 'image' );
-									echo '<h2>' . $title . '</h2>';
-									echo wp_get_attachment_image( $image, 'full', '', array( 'class' => 'mb-3' ) );
-
-									// Case: Image layout.
-								elseif ( get_row_layout() == 'video_content' ) :
-									$title = get_sub_field( 'title' );
-									$video = get_sub_field( 'video' );
-									echo '<h2>' . $title . '</h2>';
-									echo '<div class="embed-container mb-3">';
-									echo $video;
-									echo '</div>';
-
-								endif;
-							endwhile;
-						endif;
+								} else {
+										echo '<h2>' . esc_textarea( get_sub_field( 'title' ) ) . '</h2>';
+									if ( 'image_content' === get_row_layout() ) {
+										$image = get_sub_field( 'image' );
+										echo wp_get_attachment_image( $image, 'full', '', array( 'class' => 'mb-3' ) );
+									} elseif ( 'video_content' === get_row_layout() ) {
+										$video = get_sub_field( 'video' );
+										echo '<div class="embed-container mb-3">';
+										echo $video;
+										echo '</div>';
+									}
+								}
+							}
+						}
 						?>
 						<!-- .entry-content -->
 					</div>
