a5190a48bd561a9c496d74b54077de075b897fc3
KJ Roelke <kroelke@choctawnation.com>
2025-06-02 10:36:23 -0500

    staff button bug fix

--- wp-content/themes/ATIP/template-parts/archive-news-preview.php
+++ wp-content/themes/ATIP/template-parts/archive-news-preview.php
@@ -9,11 +9,11 @@
 if ( ! have_posts() ) {
 	return;
 }
-$is_even_index = true;
 ?>
 <ul class="container list-unstyled d-flex flex-column row-gap-5 align-items-stretch mb-5" style="padding-left:calc(var(--bs-gutter-x)*.5);">
 	<?php while ( have_posts() ) : ?>
 	<?php the_post(); ?>
+	<?php $is_even_index = $wp_query->current_post % 2 === 0; ?>
 	<li class="row position-relative row-gap-4 align-items-stretch" data-aos="<?php echo $is_even_index ? 'zoom-out-left' : 'zoom-out-right'; ?>">
 		<?php if ( has_post_thumbnail() ) : ?>
 		<div class="col-lg-5">
@@ -36,6 +36,5 @@ $is_even_index = true;
 			<a class="btn btn-primary btn-lg mt-auto align-self-start stretched-link" href="<?php the_permalink(); ?>">Read More</a>
 		</div>
 	</li>
-	<?php $is_even_index = ! $is_even_index; ?>
 	<?php endwhile; ?>
 </ul>
--- wp-content/themes/ATIP/template-parts/archive-staff-preview.php
+++ wp-content/themes/ATIP/template-parts/archive-staff-preview.php
@@ -8,11 +8,12 @@
 if ( ! have_posts() ) {
 	return;
 }
-$is_even_index = true; ?>
+?>
 <section class="container">
 	<div class="row row-cols-1 row-cols-lg-2 row-cols-xl-3 justify-content-center align-items-stretch row-gap-4 mb-5">
 		<?php while ( have_posts() ) : ?>
 			<?php the_post(); ?>
+			<?php $is_even_index = $wp_query->current_post % 2 === 0; ?>
 		<div class="col d-flex flex-column align-items-stretch position-relative" data-aos="<?php echo $is_even_index ? 'zoom-out-left' : 'zoom-out-right'; ?>">
 			<?php
 			if ( has_post_thumbnail() ) {
@@ -29,10 +30,9 @@ $is_even_index = true; ?>
 			<div class="d-flex flex-column flex-grow-1">
 				<?php the_title( '<h2 class="text-transform-none news-content-preview">', '</h2>' ); ?>
 				<p><?php the_field( 'archive_content' ); ?></p>
-				<a class="btn btn-primary btn-lg text-light mt-auto stretched-link align-self-start" href="<?php the_permalink(); ?>">Read Full Bio</a>
+				<a class="btn btn-primary btn-lg mt-auto stretched-link align-self-start" href="<?php the_permalink(); ?>">Read Full Bio</a>
 			</div>
 		</div>
-			<?php $is_even_index = ! $is_even_index; ?>
 		<?php endwhile; ?>
 	</div>
 </section>
